d6329dc5aef2af554261a3963f424b5257c2d769
Matthew Zhang <matthew.zhang@mail.utoronto.ca>
2018-01-02 18:54:12 -0500

    Update p_ws.py

--- p_ws.py
+++ p_ws.py
@@ -7,14 +7,14 @@ import urllib.request as ul
 import re
 
 # Defines the webstring class, which fetches strings from the web based on specified inputs
-#----------------------------- WARNING NOT FULLY DEBUGGED
+
 # Define class web_string which stores Chinese string
 # Attributes: url, chapters (optional), contents
 
 class c_webstring:
     
     # Initialize Object
-    def init(self, url_root, url_terminal = None, chapters = 0, curr_chapter = 0):
+    def __init__(self, url_root, chapter = 0, url_terminal = None, curr_chapter = 1):
         self.root = url_root # Root address
         self.chapter = chapter # Optional address terminus
         self.term = url_terminal # Chapter integer
@@ -23,12 +23,12 @@ class c_webstring:
         self.content = [] # Url Contents
         return
         
-    def modify(self, url_root = None, url_terminal = None, chapters = None, curr_chapter = None):
+    def modify(self, url_root = None, chapter = None, url_terminal = None, curr_chapter = None):
         if url_root != None:
             self.root = url_root
         if url_terminal != None:
             self.term = url_terminal
-        if chapters != None:
+        if chapter != None:
             self.chapter = chapter
         if curr_chapter != None:
             self.curr_chapter = curr_chapter
@@ -37,57 +37,73 @@ class c_webstring:
     def extract_all(self, body_head, body_tail):
         #If no chapters specified
         if self.chapter == 0:
-            full = ul.urlopen(url_root) # Get full source code
-            head = full.index(body_head) # Fetch index of body_heat string
+            response = ul.urlopen(url_root) # Get full source code
+            full = response.read().decode('utf-8')
+            for i in range(len(body_head)):
+                try:
+                    head = full.index(body_head[i]) # Fetch index of body_heat string
+                except:
+                    continue
             tail = full.index(body_tail) # Fetch index of body_tail string
             self.content.append(full[head, tail + len(body_tail)]) # Splicing and appending
             
         #If integer range specified    
         else:
-            for i in range(self.curr_chapter, self.chapter):
-                try: 
-                    full = ul.urlopen(entry_string.root + i + entry_string.term) # Getting address and source code
-                    head = full.index(body_head) # Fetch index of body_heat string
-                    tail = full.index(body_tail) # Fetch index of body_tail string
-                    self.contents.append(full[head, tail + len(body_tail)]) # Splicing and appending
-                except:
-                    print("Unspecified error, check url names and page text encoding")
+            for i in range(self.curr_chapter, self.chapter + 1):
+                response = ul.urlopen(self.root + str(i) + self.term) # Getting address and source code
+                full = response.read().decode('utf-8')
+                for j in range(len(body_head)):
+                    try:
+                        head = full.index(body_head[j]) # Fetch index of body_heat string
+                    except(ValueError):
+                        continue
+                tail = full.index(body_tail) # Fetch index of body_tail string
+                self.content.append(full[head:tail + len(body_tail)]) # Splicing and appending
+                self.curr_chapter += 1
     
-    def clean_all(self, html_flag = 1, ws_flag = 1, punct_flag = 1, html_markers = None, punct_markers = None):
+    def clean_all(self, html_flag = 0, ws_flag = 0, punct_flag = 0, strip_chinese = 1, html_markers = None, punct_markers = None):
         if html_flag == 1:
-            clean_html(self, html_markers) # Removes html text modifiers listed in html_markers; default = all modifiers
+            self.clean_html(html_markers) # Removes html text modifiers listed in html_markers; default = all modifiers
         if ws_flag == 1:
-            clean_ws(self) # Removes white spaces
+            self.clean_ws() # Removes white spaces
         if punct_flag == 1:
-            clean_punct(self, punct_markers) # Removes punctuation listed in punct_markers, default = all punctuation
+            self.clean_punct(punct_markers) # Removes punctuation listed in punct_markers, default = all punctuation
+        if strip_chinese == 1:
+            self.strip_chinese()
         
     def clean_html(self, html_markers):
         if html_markers == None:
-            for i in range(len(self.contents)):
-                self.contents[i] = re.sub("[\<].*?[\>]", "", self.contents[i], re.UNICODE) # Deletes all html text modifiers
+            for i in range(len(self.content)):
+                self.content[i] = re.sub("[\<].*?[\>]", "", self.content[i], re.UNICODE) # Deletes all html text modifiers
                 
         else: # Expect html_markers to be list
-            for i in range(len(self.contents)):
+            for i in range(len(self.content)):
                 for j in range(len(html_markers)):
-                    self.contents[i] = self.contents[i].replace(html_markers[j], "") # Deletes all modifiers listed in html_markers
+                    self.content[i] = self.contents[i].replace(html_markers[j], "") # Deletes all modifiers listed in html_markers
         return        
         
     def clean_ws(self):
-        for i in range(len(self.contents)):
+        for i in range(len(self.content)):
             regex = re.compile(r"\s+") # 
-            self.contents[i] = re.sub(regex, '', sentence, re.UNICODE)
+            self.content[i] = re.sub(regex, '', self.content[i], re.UNICODE)
         return
 
     def clean_punct(self, punct_markers):
         if punct_markers == None:
-            for i in range(len(self.contents)):
-                self.contents[i] = re.sub(r'[^\w\s]','',self.contents[i], re.UNICODE)
+            for i in range(len(self.content)):
+                self.content[i] = re.sub(r'[^\w\s]','',self.content[i], re.UNICODE)
                 
         else: # Expect punct to be list
-            for i in range(len(self.contents)):
+            for i in range(len(self.content)):
                 for j in range(len(punct_markers)):
-                    self.contents[i] = self.contents[i].replace(punct_markers[j], "")
+                    self.content[i] = self.content[i].replace(punct_markers[j], "")
         return
                 
+    def strip_chinese(self):
+        for i in range(len(self.content)):
+            regex = re.compile(u'[^\u4E00-\u97A5]')
+            self.content[i] = regex.sub(r'',self.content[i])
+            self.content[i] = re.sub("\s","",self.content[i])                   
+    
     def to_utf8(self):
         return # will code if necessary
